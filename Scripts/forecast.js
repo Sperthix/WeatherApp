4765e9308df2be39dd5e99dfb21094e1801c38ac
Sperthix <matusbuci@gmail.com>
2021-07-31 11:25:28 +0200

    added query for getting city key and current weather for that key

--- Scripts/forecast.js
+++ Scripts/forecast.js
@@ -1,1 +1,35 @@
 const key = 'j5qtYdnKCN2RytrdsfgHeGx5kbu0xxiy';
+
+//getting city Key
+const getCity = async city => {
+    const base  = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+}
+
+//getting weather for city using cityKey
+const getWeather = async cityID => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${cityID}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+}
+
+getWeather('297345');
+
+
+getCity('Bratislava')
+    .then(data => getWeather(data.Key))
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
